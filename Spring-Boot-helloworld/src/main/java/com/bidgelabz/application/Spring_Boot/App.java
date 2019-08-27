aa1dfb98fc61e693e3e9a7858fed5f920982052f
chandrakishore314 <chandrakishore314@gmail.com>
2019-08-27 12:55:08 +0530

    first commit

---
+++ Spring-Boot-helloworld/src/main/java/com/bidgelabz/application/Spring_Boot/App.java
@@ -0,0 +1,13 @@
+package com.bidgelabz.application.Spring_Boot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
